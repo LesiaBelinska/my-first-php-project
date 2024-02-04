3d98b8506bada3c9a809c70c8472079385dc977c
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-04 14:15:08 +0200

    hw-06 done

---
+++ homework/hw-06.php
@@ -0,0 +1,39 @@
+<?php
+
+function circleAreaCalculation($radius)
+{
+    $pi = pi();
+
+    $area = $radius * $pi;
+    return $area;
+}
+
+$radius = 10;
+$square = circleAreaCalculation($radius);
+
+echo "Площа кола з радіусом $radius дорівнює $square." . PHP_EOL;
+
+
+//переприсвоєння
+
+function power(&$number, $pow)
+{
+    $number **= $pow;
+}
+
+$number = 10;
+power($number, pow: 2);
+
+echo $number . PHP_EOL;
+
+
+// повернення
+function power2($number, $pow)
+{
+    return $number ** $pow;
+}
+
+$number = 5;
+$number = power2($number, 3);
+
+echo $number . PHP_EOL;
