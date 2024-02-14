6d76998be43e473cada6ce65206c513d28db962a
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-14 08:44:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('APP_DIR', __DIR__ . '/');
+define('LOG_DIR', __DIR__ . '/logs/');
+define('LOG_FILE', 'logs.txt');
+
+require APP_DIR . 'functions.php';
+
+Logger('call index file test', 'action');
