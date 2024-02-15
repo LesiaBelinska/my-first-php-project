98df5a6866fa4ecf01d57b4726d074a94280158a
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-15 16:59:38 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,14 @@
 <?php
 declare(strict_types=1);
 
+// for own experience
 /**
- * This function can read files
+ * This function takes a file name as input and returns a generator
+ * that yields the content of the file if it exists.
  * @param string $fileName
  * @return Generator
  */
+
 function fileReadContentGenerator(string $fileName): Generator
 {
     if (file_exists($fileName)) {
@@ -14,42 +17,36 @@ function fileReadContentGenerator(string $fileName): Generator
 }
 
 /**
+ * Function reads the content of a file and returns the last line of the file as a string.
+ * If the file does not exist or is empty, it returns null.
  * @param string $fileName
  * @return string|null
  */
 function fileReadLastLine(string $fileName): ?string
 {
-    $file = fopen($fileName, 'r');
-    $lastLine = null;
-    while (($line = fgets($file)) !== false) {
-        $lastLine = $line;
-    }
-    fclose($file);
-    return $lastLine;
-}
-
-/**
- * @param string $fileName
- * @return string|null
- */
-function fileReadLastLine2(string $fileName): ?string
-{
     if (file_exists($fileName)) {
-        $lines = file($fileName);
-        return end($lines);
+        $file = fopen($fileName, 'r');
+        $lastLine = null;
+        while (($line = fgets($file)) !== false) {
+            $lastLine = $line;
+        }
+        fclose($file);
+        return $lastLine;
     }
     return null;
 }
 
-
 /**
+ * Function appends the message to the end of the file specified by the file name.
  * @param string $fileName
  * @param string $message
  * @return void
  */
 function fileAddContent(string $fileName, string $message): void
 {
-    file_put_contents($fileName, $message . "\n", FILE_APPEND);
+    if (file_exists($fileName)) {
+        file_put_contents($fileName, $message . "\n", FILE_APPEND);
+    }
 }
 
 
