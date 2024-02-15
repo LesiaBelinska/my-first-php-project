7bdb703299a7d04a3dc634b855ad6ae32e5109f0
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-15 10:15:29 +0200

    Update hw-11.php

--- homework/hw-11.php
+++ homework/hw-11.php
@@ -4,14 +4,16 @@ require_once  APP_DIR . 'functions.php';
 
 $fileName = APP_DIR . 'text.txt';
 
-$contents = fileReadContentGenerator($fileName);
-
-foreach ($contents as $content) {
-    echo $content . PHP_EOL;
-}
-
 echo 'Enter your message below' . PHP_EOL;
 $message = trim(fgets(STDIN));
 fileAddContent($fileName, $message);
 
-Logger('call homework-11 file', 'action');
+
+//$contents = fileReadContentGenerator($fileName);
+//
+//foreach ($contents as $content) {
+//    echo $content . PHP_EOL;
+//}
+
+$lastLine = fileReadLastLine($fileName);
+echo "You entered: $lastLine";
