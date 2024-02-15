16361b21e96cfd1fc20b5290d3acb926f7af2a20
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-15 16:59:45 +0200

    Update LoggerFunction.php

--- LoggerFunction.php
+++ LoggerFunction.php
@@ -2,6 +2,10 @@
 declare(strict_types=1);
 
 /**
+ * logs a message with a specified type to a log file.
+ * It formats the message with the type, current date and time, and the provided message.
+ * The log file is specified by the optional parameter file, with a default value of LOG_FILE.
+ * It returns true if the logging operation is successful, and false otherwise.
  * @param string $message
  * @param string $type
  * @param string $file
