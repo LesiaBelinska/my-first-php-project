144f75c93e39d981ddad6d418758d0368ee2ccfb
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-17 19:18:07 +0200

    Update hw-12.php

--- homework/hw-12.php
+++ homework/hw-12.php
@@ -2,17 +2,25 @@
 
 require __DIR__ . '/../index.php';
 require APP_DIR . 'classes/Worker.php';
+require APP_DIR . 'enums/WorkPositions.php';
+
 //$worker = new Worker(); // without __construct
 //$worker->setName("Jon");
 //$worker->setPosition("developer");
 
 try {
-    $worker = new Worker("J", "developer");
-    $worker2 = new Worker("Kate", "manager");
+    $worker = new Worker("Jon", "developer");
+//    $worker2 = new Worker("Kate", "manager");
 } catch (Exception $exception){
     echo $exception->getMessage();
 }
 
+try {
+//    $worker = new Worker("Jon", "developer");
+    $worker2 = new Worker("Kate", "manager");
+} catch (Exception $exception){
+    echo $exception->getMessage();
+}
 
 //Виклик методів
  function showWorkerInformation(Worker $worker): void
@@ -20,5 +28,7 @@ try {
      echo "name: " . $worker->getName() . PHP_EOL;
      echo "position: " . $worker->getPosition() . PHP_EOL;
  }
+
  showWorkerInformation($worker);
  showWorkerInformation($worker2);
+
