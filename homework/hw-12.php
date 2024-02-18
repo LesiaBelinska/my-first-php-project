b139b99ec4024efa03c087f26a44f1f9c2190dab
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-18 18:36:58 +0200

    Update hw-12.php

--- homework/hw-12.php
+++ homework/hw-12.php
@@ -10,25 +10,26 @@ require APP_DIR . 'enums/WorkPositions.php';
 
 try {
     $worker = new Worker("Jon", "developer");
-//    $worker2 = new Worker("Kate", "manager");
-} catch (Exception $exception){
+} catch (Exception $exception) {
     echo $exception->getMessage();
 }
 
 try {
-//    $worker = new Worker("Jon", "developer");
-    $worker2 = new Worker("Kate", "manager");
-} catch (Exception $exception){
+    $worker2 = new Worker("Kate", "QA");
+} catch (Exception $exception) {
     echo $exception->getMessage();
 }
 
-//Виклик методів
- function showWorkerInformation(Worker $worker): void
- {
-     echo "name: " . $worker->getName() . PHP_EOL;
-     echo "position: " . $worker->getPosition() . PHP_EOL;
- }
+function showWorkerInformation(Worker $worker): void
+{
+    echo "name: " . $worker->getName() . PHP_EOL;
+    echo "position: " . $worker->getPosition() . PHP_EOL;
+}
 
- showWorkerInformation($worker);
- showWorkerInformation($worker2);
+if (isset($worker)) {
+    showWorkerInformation($worker);
+}
 
+if (isset($worker2)) {
+    showWorkerInformation($worker2);
+}
