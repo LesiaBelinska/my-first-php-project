5aecea949ed4ef11369b787a7b23bc0004564f4d
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-20 22:36:35 +0200

    Create hw-14.php

---
+++ homework/hw-14.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . '/../index.php';
+require APP_DIR . 'classes/BankAccount.php';
+
+try {
+    $bankAccount = new BankAccount(12345678, 1000);
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
+
+if (isset($bankAccount)) {
+    $bankAccount->showAccountInfo();
+}
+
+$bankAccount->deposit(500);
+$bankAccount->withdraw(200);
+
+$bankAccount->withdraw(10000);
+
+$bankAccount->withdraw(0);
+$bankAccount->deposit(0);
+$bankAccount->deposit(500000);
