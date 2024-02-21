6c731ef5334cc228a2cbb880821d5ddb6704ad5a
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-21 21:41:05 +0200

    Update hw-14.php

--- homework/hw-14.php
+++ homework/hw-14.php
@@ -3,6 +3,10 @@
 require __DIR__ . '/../index.php';
 require APP_DIR . 'classes/BankAccount.php';
 
+$messageHandler = new MessageHandler();
+
+var_dump($messageHandler);
+
 try {
     $bankAccount = new BankAccount(12345678, 1000);
 } catch (Exception $exception) {
@@ -18,6 +22,8 @@ try {
     $bankAccount->deposit(500);
 } catch (Exception $exception) {
     echo $exception->getMessage();
+} finally {
+
 }
 
 try {
@@ -49,3 +55,60 @@ try {
 } catch (Exception $exception) {
     echo $exception->getMessage();
 }
+
+
+
+
+
+
+
+//
+//require __DIR__ . '/../index.php';
+//require APP_DIR . 'classes/BankAccount.php';
+//
+//try {
+//    $bankAccount = new BankAccount(12345678, 1000);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//
+//if (isset($bankAccount)) {
+//    $bankAccount->showAccountInfo();
+//}
+//
+//try {
+//    $bankAccount->deposit(500);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//try {
+//    $bankAccount->withdraw(200);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//try {
+//    $bankAccount->withdraw(10000);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//try {
+//    $bankAccount->withdraw(0);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//try {
+//    $bankAccount->deposit(0);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
+//
+//try {
+//    $bankAccount->deposit(500000);
+//} catch (Exception $exception) {
+//    echo $exception->getMessage();
+//}
