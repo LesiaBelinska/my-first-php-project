dfeaea1e6b5b40520245279379c3a1ef19563b9a
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-21 21:41:07 +0200

    Update BankAccount.php

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -32,6 +32,9 @@ class BankAccount
      */
     public function setBalance(int $balance): void
     {
+        if ($balance < 0) { //додала
+            throw new Exception("Invalid balance amount." . PHP_EOL);
+        }
         $this->balance = $balance;
     }
 
@@ -43,21 +46,25 @@ class BankAccount
         return $this->balance;
     }
 
-    public function deposit(int $amount): void
+    public function deposit(int $amount): bool // змінено з void на bool
     {
-        $this->validateAmount($amount, 'deposit');
+//        $this->validateAmount($amount, 'deposit');
+        $this->validateAmount($amount);
         $this->balance += $amount;
-        $this->showMessage("Account has been credited with $amount USD. New balance: {$this->balance}");
+//        $this->showMessage("Account has been credited with $amount USD. New balance: {$this->balance}");
+        return true; // додала
     }
 
-    public function withdraw(int $amount): void
+    public function withdraw(int $amount): bool // змінено з void на bool
     {
-        $this->validateAmount($amount, 'withdraw');
+//        $this->validateAmount($amount, 'withdraw');
+        $this->validateAmount($amount);
         if ($amount > $this->balance) {
             throw new Exception('Not enough funds in the account.' . PHP_EOL);
         }
         $this->balance -= $amount;
-        $this->showMessage("Withdrew $amount USD. New balance: {$this->balance} USD.");
+//        $this->showMessage("Withdrew $amount USD. New balance: {$this->balance} USD.");
+        return true; // додала
     }
 
     private function validateAmount(int $amount, string $purpose): void
@@ -67,10 +74,10 @@ class BankAccount
         }
     }
 
-    private function showMessage(string $message): void
-    {
-        echo $message . PHP_EOL;
-    }
+//    private function showMessage(string $message): void
+//    {
+//        echo $message . PHP_EOL;
+//    }
 
     public function showAccountInfo(): void
     {
@@ -78,3 +85,88 @@ class BankAccount
         echo "Balance: {$this->getBalance()}" . PHP_EOL;
     }
 }
+
+
+
+
+
+
+
+//class BankAccount
+//{
+//    private int $accountNumber;
+//    private int $balance;
+//
+//    public function __construct(int $accountNumber, int $balance)
+//    {
+//        $this->setAccountNumber($accountNumber);
+//        $this->setBalance($balance);
+//    }
+//
+//    /**
+//     * @param int $accountNumber
+//     */
+//    public function setAccountNumber(int $accountNumber): void
+//    {
+//        $this->accountNumber = $accountNumber;
+//    }
+//
+//    /**
+//     * @return int
+//     */
+//    public function getAccountNumber(): int
+//    {
+//        return $this->accountNumber;
+//    }
+//
+//    /**
+//     * @param int $balance
+//     */
+//    public function setBalance(int $balance): void
+//    {
+//        $this->balance = $balance;
+//    }
+//
+//    /**
+//     * @return int
+//     */
+//    public function getBalance(): int
+//    {
+//        return $this->balance;
+//    }
+//
+//    public function deposit(int $amount): void
+//    {
+//        $this->validateAmount($amount, 'deposit');
+//        $this->balance += $amount;
+//        $this->showMessage("Account has been credited with $amount USD. New balance: {$this->balance}");
+//    }
+//
+//    public function withdraw(int $amount): void
+//    {
+//        $this->validateAmount($amount, 'withdraw');
+//        if ($amount > $this->balance) {
+//            throw new Exception('Not enough funds in the account.' . PHP_EOL);
+//        }
+//        $this->balance -= $amount;
+//        $this->showMessage("Withdrew $amount USD. New balance: {$this->balance} USD.");
+//    }
+//
+//    private function validateAmount(int $amount, string $purpose): void
+//    {
+//        if ($amount <= 0) {
+//            throw new Exception("Invalid amount for $purpose." . PHP_EOL);
+//        }
+//    }
+//
+//    private function showMessage(string $message): void
+//    {
+//        echo $message . PHP_EOL;
+//    }
+//
+//    public function showAccountInfo(): void
+//    {
+//        echo "Bank account number: {$this->getAccountNumber()}" . PHP_EOL;
+//        echo "Balance: {$this->getBalance()}" . PHP_EOL;
+//    }
+//}
