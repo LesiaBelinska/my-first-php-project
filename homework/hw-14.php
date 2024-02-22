f28d7b95995bce5c3deaa197abbada77c6c1db72
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-22 11:27:23 +0200

    Update hw-14.php

--- homework/hw-14.php
+++ homework/hw-14.php
@@ -4,27 +4,21 @@ require __DIR__ . '/../index.php';
 require APP_DIR . 'classes/BankAccount.php';
 require APP_DIR . 'classes/MessageHandler.php';
 
-$messageHandler = new MessageHandler();
-
 
 try {
     $bankAccount = new BankAccount(12345678, 1000);
 } catch (Exception $exception) {
     echo $exception->getMessage();
+    exit;
 }
 
-
-if (isset($bankAccount)) {
-    $bankAccount->showAccountInfo();
-}
+$bankAccount->showAccountInfo();
 
 try {
-   if (isset($bankAccount)) {  // ?????? чи потрібно тут використовувати перевірку?????
-       $amount = 500;
-       $bankAccount->deposit($amount);
-       $newBalance = $bankAccount->getBalance();
-       MessageHandler::showMessage("Account has been credited with $amount USD. New balance: $newBalance USD");
-   }
+    $amount = 500;
+    $bankAccount->deposit($amount);
+    $newBalance = $bankAccount->getBalance();
+    MessageHandler::showMessage("Account has been credited with $amount USD. New balance: $newBalance USD");
 } catch (Exception $exception) {
     echo $exception->getMessage();
 }
@@ -46,7 +40,6 @@ try {
 } catch (Exception $exception) {
     echo $exception->getMessage();
 }
-
 
 try {
     $amount = 0;
@@ -74,64 +67,3 @@ try {
 } catch (Exception $exception) {
     echo $exception->getMessage();
 }
-
-
-if (isset($bankAccount)) {
-    $bankAccount->showAccountInfo();
-}
-
-
-
-
-
-
-//
-//require __DIR__ . '/../index.php';
-//require APP_DIR . 'classes/BankAccount.php';
-//
-//try {
-//    $bankAccount = new BankAccount(12345678, 1000);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//
-//if (isset($bankAccount)) {
-//    $bankAccount->showAccountInfo();
-//}
-//
-//try {
-//    $bankAccount->deposit(500);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//try {
-//    $bankAccount->withdraw(200);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//try {
-//    $bankAccount->withdraw(10000);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//try {
-//    $bankAccount->withdraw(0);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//try {
-//    $bankAccount->deposit(0);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
-//
-//try {
-//    $bankAccount->deposit(500000);
-//} catch (Exception $exception) {
-//    echo $exception->getMessage();
-//}
