641b465749217ec71dafac7f6e448724930ea693
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-25 16:33:49 +0200

    Update hw-13.php

--- homework/hw-13.php
+++ homework/hw-13.php
@@ -1,13 +1,10 @@
 <?php
 
 require __DIR__ . '/../index.php';
-require APP_DIR . 'classes/Worker.php';
-require APP_DIR . 'enums/WorkPositions.php';
+require APP_DIR . 'classes/TaskTracker.php';
+require APP_DIR . 'enums/TaskStatus.php';
 require APP_DIR . 'enums/Priority.php';
 
-try {
-    $taskList = new TaskTracker("tasks.txt");
-} catch (error) {
-    echo "ERROR!!!!!";
-    exit;
-}
+$fileName = APP_DIR . 'files/tasks.txt';
+
+$taskList = new TaskTracker($fileName);
