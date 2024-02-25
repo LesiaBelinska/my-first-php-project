5f948ce657b8b54df8c55f2abbe95fe88402197a
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-25 20:10:29 +0200

    Update hw-13.php

--- homework/hw-13.php
+++ homework/hw-13.php
@@ -7,4 +7,24 @@ require APP_DIR . 'enums/Priority.php';
 
 $fileName = APP_DIR . 'files/tasks.txt';
 
-$taskList = new TaskTracker($fileName);
+try {
+    $taskList = new TaskTracker($fileName);
+} catch (error) {
+    echo "ERROR!!!!!" . PHP_EOL;
+    exit;
+}
+
+$taskList->addTask("do homework", Priority::HIGH);
+$taskList->addTask("buy coffee", Priority::LOW);
+$taskList->addTask("buy tickets to London", Priority::MIDDLE);
+$taskList->addTask("go to the gym", Priority::MIDDLE);
+$taskList->addTask("call to mom", Priority::LOW);
+$taskList->addTask("do donate", Priority::HIGH);
+$taskList->completeTask('');
+$taskList->deleteTask('');
+
+$myTasks = $taskList->getTasks();
+$taskList->displayTasks($myTasks);
+
+
+
