49d54cd47c6db6e0753cddb64f0b6a03f870328f
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-27 11:48:15 +0200

    Create TextPrinter.php

---
+++ classes/TextPrinter.php
@@ -0,0 +1,23 @@
+<?php
+
+class TextPrinter {
+    protected string $text;
+
+    public function __construct(string $text = "some text")
+    {
+        if (empty($text)) {
+            throw new InvalidArgumentException('Text cannot be empty');
+        }
+        $this->text = $text;
+    }
+
+    public function print(): string
+    {
+        return ucfirst($this->text);
+    }
+
+    public function showText(): void
+    {
+        echo $this->print() . PHP_EOL;
+    }
+}
