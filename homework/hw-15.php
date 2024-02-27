63a8feb4f733942789d3b9457a936c5bc685c7fe
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-27 12:42:09 +0200

    Update hw-15.php

--- homework/hw-15.php
+++ homework/hw-15.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../index.php';
 require APP_DIR . 'classes/TextPrinter.php';
 require APP_DIR . 'classes/UpperCaseTextPrinter.php';
+require APP_DIR . 'functions.php';
 
 try {
     $baseObject = new TextPrinter();
@@ -11,8 +12,6 @@ try {
     exit;
 }
 
-$baseObject->showText();
-
 try {
     $subObject = new UpperCaseTextPrinter();
 } catch (InvalidArgumentException $e) {
@@ -20,4 +19,5 @@ try {
     exit;
 }
 
-$subObject->showText();
+showText($baseObject);
+showText($subObject);
