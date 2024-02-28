5d1e09177a9e6715abadb41a895072727ac3dd43
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-02-28 22:40:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ define('APP_DIR', __DIR__ . '/');
 define('LOG_DIR', __DIR__ . '/logs/');
 define('LOG_FILE', 'logs.txt');
 
-require APP_DIR . 'system/Logger.php';
-require APP_DIR . 'enums/LogLevels.php';
+//require APP_DIR . 'system/Logger.php';
+//require APP_DIR . 'enums/LogLevels.php';
 
 //require APP_DIR . 'loggerFunction.php';
 
 //Logger('call index file test', 'action');
 
-Logger::log('test message2');
-Logger::action("test message3");
+//Logger::log('test message');
+//Logger::action('action');
+//Logger::error('error');
+
