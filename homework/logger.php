ce39a3c6a3cd67d3ea5f76c20eef34b12b5983a2
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-03-13 21:45:37 +0200

    Create logger.php

---
+++ homework/logger.php
@@ -0,0 +1,17 @@
+<?php
+//
+//require __DIR__ . '/../index.php';
+//
+//define('LOG_DIR', __DIR__ . '/logs/');
+//define('LOG_FILE', 'logs.txt');
+//
+//require APP_DIR . 'system/Logger.php';
+//require APP_DIR . 'enums/LogLevels.php';
+//
+////require APP_DIR . 'LoggerFunction.php';
+//
+//Logger('call index file test', 'action');
+//
+//Logger::log('test message');
+//Logger::action('action');
+//Logger::error('error');
