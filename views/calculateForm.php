e7e0f7f756daa06f769499a40a36955d777aa580
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-03-13 21:46:50 +0200

    Create calculateForm.php

---
+++ views/calculateForm.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculate Form</title>
+</head>
+<body>
+ <form action="/calculate" method="post">
+     <div>
+         <label for="first-number">First number</label>
+         <input type="number" name="first-number" id="first-number"/>
+     </div>
+     <div>
+         <label for="second-number">Second number</label>
+         <input type="number" name="second-number" id="second-number"/>
+     </div>
+     <button type="submit">Calculate sum</button>
+ </form>
+</body>
+</html>
