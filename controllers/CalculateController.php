eb4aac697e028b136a658d19186b2ed3a40787cf
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-03-14 11:42:52 +0200

    Update CalculateController.php

--- controllers/CalculateController.php
+++ controllers/CalculateController.php
@@ -2,8 +2,23 @@
 
 class CalculateController
 {
-    public function calculate()
+    use NumberValidator;
+    public function calculate(): bool
     {
         return view('calculateForm.php');
     }
+
+    public function calculateResult(): bool
+    {
+        $firstNumber = isset($_POST['first-number']) ? intval($_POST['first-number']) : 0;
+        $secondNumber = isset($_POST['second-number']) ? intval($_POST['second-number']) : 0;
+
+        if(!$this->validateNumber($firstNumber) || !$this->validateNumber($secondNumber)){
+            throw new Exception('Invalid input data');
+        }
+
+        $sum = $firstNumber + $secondNumber;
+        return view('calculateResult.php', ['sum' => $sum]);
+    }
+
 }
