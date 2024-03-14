fd604f17e9205b591a048f93128bb42a04efe905
lesia.belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-03-14 12:57:47 +0200

    Update calculateResult.php

--- views/calculateResult.php
+++ views/calculateResult.php
@@ -6,8 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Calculate Result</title>
+    <link href="../css/main.css" rel="stylesheet"/>
 </head>
 <body>
-    <h1>Sum of two numbers is: <?= $sum ?></h1>
+    <div class="wrapper">
+        <?php if (isset($sum)): ?>
+            <h1 class="sum-title">Sum of two numbers is: <span><?= $sum ?></span></h1>
+        <?php endif; ?>
+    </div>
 </body>
 </html>
